44284d0eb9c849e36f7c1b413c39bb63633b83a6
Luan Young <luanpy@gmail.com>
2026-07-18 13:57:55 -0300

    add joystick teleop to brutusbot_controller pkg

---
+++ brutusbot_controller/launch/joystick_teleop.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        name="use_sim_time",
+        default_value="True",
+        description="Use simulated time"
+    )
+
+    joy_node = Node(
+        package="joy",
+        executable="game_controller_node",
+        name="joystick",
+        parameters=[
+            os.path.join(get_package_share_directory("brutusbot_controller"), "config", "joy_config.yaml"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")}
+        ]
+    )
+
+    joy_teleop = Node(
+        package="joy_teleop",
+        executable="joy_teleop",
+        parameters=[
+            os.path.join(get_package_share_directory("brutusbot_controller"), "config", "joy_teleop.yaml"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")}
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            use_sim_time_arg,
+            joy_node,
+            joy_teleop,
+        ]
+    )
